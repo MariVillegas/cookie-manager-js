222b8d77816b8dc3816ea474ec0c9350f3fe130d
Mariana <mva.mari29@gmail.com>
2023-04-27 12:04:40 -0600

    Add files

---
+++ index.js
@@ -0,0 +1,28 @@
+
+window.onload = function() {
+
+    // CookieManager.set('Alias', 'Mari', 3);
+    // CookieManager.set('Alias1', 'D', 3);
+    // CookieManager.set('Alias2', 'Andre', 3);
+    // CookieManager.set('Alias3', 'Ilia', 3);
+    // CookieManager.set('Alias4', 'Cali', 3);
+    // CookieManager.set('Alias5', 'Memo', 3);
+
+    // console.log(CookieManager.get('Alias'));
+    // console.log(CookieManager.get('Alias3'));
+    // console.log(CookieManager.get('Alias1'));
+    // console.log(CookieManager.get('sdsd'));
+
+
+    // CookieManager.update('Alias', 'Marie', 30);
+    // CookieManager.update('Alias1', 'Ilia', 5);
+    // CookieManager.update('Alias3', 'D', 5);
+
+    //CookieManager.remove('Alias3');
+
+    // console.log(CookieManager.getAll());
+    // var c = CookieManager.getAll()
+    // console.log(c[0].value);
+
+    CookieManager.clear();
+};
